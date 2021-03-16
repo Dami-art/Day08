1be952b8279636882cf02431eb4f62cf62738c24
Dami-art <damjana.zoric@gmail.com>
2021-03-16 08:22:04 +0100

    kkk

---
+++ ex07/src/index.js
@@ -0,0 +1,10 @@
+//Write code bellow this line
+
+function convertStrToInt(str){
+    res =  res * num,
+}
+convertStrToInt(str);
+//Only change code above this line
+
+module.exports = convertStrToInt;
+
