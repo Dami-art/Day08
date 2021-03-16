7a7155014ef419a6d2b196eab9bcc5c5d5f1ceda
Dami-art <damjana.zoric@gmail.com>
2021-03-16 09:06:33 +0100

    ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,14 @@
+//Write code bellow this line
+var total = 0;
+
+function funcWithArg(digit) {
+    total = (digit + 9) / 5;
+}
+    funcWithArg(6);
+
+
+//Only change code above this line
+
+console.log(total);
+module.exports = funcWithArg; 
+
